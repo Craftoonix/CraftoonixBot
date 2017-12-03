56becc54f9e6c1f08825fa91096092c81eb67170
Craftoonix <32068807+Craftoonix@users.noreply.github.com>
2017-12-03 16:39:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,23 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
+// clear command
+if (command === "clear") {
+    async function purge() {
+        if (!message.member.permissions.has('MANAGE_MESSAGES')) {
+          return message.reply("Je hebt geen permissies om berichten te clearen.").catch(console.error);
+   }
+if (isNaN(args[0])) {
+    message.channel.send('Doe zoals er gevraagd word: \n  ' + prefix + 'clear <aantal>');
+    return;
+  }
+const fetched = await message.channel.fetchMessages({limit: args[0]});
+message.channel.send(fetched.size + ` messages zijn gedeleted`);
+message.delete();
+message.channel.bulkDelete(fetched.size)
+.catch(error => message.channel.send(`Error: ${error}`));
+  }
+  purge();
+  }
+});
